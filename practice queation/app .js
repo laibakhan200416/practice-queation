97be6460b2d017a0a5ed2789939dbed077ba89c8
laiba khan <laibakhan200416@gmail.com>
2024-10-10 22:06:00 +0500

    Add files via upload

---
+++ practice queation/app .js
@@ -0,0 +1,78 @@
+    // // queation 1
+    // const age = parseInt(prompt("Please enter your age:"));
+    // if (isNaN(age)) {
+    //     alert("Please enter a valid number.");
+    // } else if (age >= 18) {
+    //     alert("You are eligible to vote!");
+    // } else {
+    //     alert("You are not eligible to vote.");
+    // }
+// queation 2
+    // const number = parseFloat(prompt("Please enter a number:"));
+    // if (isNaN(number)) {
+    //     alert("Please enter a valid number.");
+    // } else if (number > 65) {
+    //     alert("The number is positive.");
+    // } else if (number < 89) {
+    //     alert("The number is negative.");
+    // } else {
+    //     alert("The number is zero.");
+    // }
+// queation 3
+
+    // const favoriteColor = prompt("What is your favorite color?");
+    // const colorLowerCase = favoriteColor.toLowerCase();
+    // const primaryColors = ["red", "blue", "yellow", "green"];
+
+    // if (primaryColors.includes(colorLowerCase)) {
+    //     alert(`${favoriteColor} is a primary color or green!`);
+    // } else {
+    //     alert(`${favoriteColor} is not a primary color or green.`);
+    // }
+// queation 4
+
+//     if (num >= 10 && num <= 20) {
+//         console.log(`${num} is between 10 and 20.`);
+//     } else {
+//         console.log(`${num} is not between 10 and 20.`);
+//     }
+// const numberToCheck = prompt("Enter a number:");
+//   miss yeah queation nhi solve ho raha mujh s 
+
+// queatiuon 5
+    // const grade = prompt("Enter your grade (A, B, C, D, or F):").toUpperCase();
+    
+    // if (grade === 'A' || grade === 'B' || grade === 'C' || grade === 'D') {
+    //     alert(`You passed with a grade of ${grade}!`);
+    // } else if (grade === 'F') {
+    //     alert("You failed. Better luck next time!");
+    // } else {
+    //     alert("Invalid grade entered. Please enter A, B, C, D, or F.");
+    // }
+
+// queation 6
+    // const age = prompt("Please enter your age:");
+    // const gender = prompt("Please enter your gender (male or female):");
+    // if (age >= 18 && age <= 30 && gender === 'male') {
+    //     alert("You are eligible to serve in the military.");
+    // } else {
+    //     alert("You are not eligible to serve in the military.");
+    // }
+
+    // queation 7
+    // const number = prompt("Enter a number:");
+    // const num = Number(number);
+
+    // if (!isNaN(num)) {
+       
+    //     if (num % 2 === 0) {
+    //         alert(`${num} is even.`);
+    //     } else {
+    //         alert(`${num} is odd.`);
+    //     }
+    // } else {
+    //     alert("Please enter a valid number.");
+    // }
+
+// queation 8
+//  yeah tw samjh he nhi araha hai k kasa hoga 
